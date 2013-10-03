f93df8b92684fa41cf0f956c17de3eb1acc01021
aelaguiz@gmail.com <aelaguiz@gmail.com>
2013-10-03 14:21:51 -0500

    Stuff

--- tpred/reports/ducksboard_reports.py
+++ tpred/reports/ducksboard_reports.py
@@ -12,3 +12,6 @@ if __name__ == '__main__':
 
     data = list(trending_posts.run_report(15, False))
     ducksboard.timeline(data, 237985)
+
+    data = list(trending_posts.run_report(60, True))
+    ducksboard.timeline(data, 238008)
--- tpred/reports/trending_posts.py
+++ tpred/reports/trending_posts.py
@@ -2,8 +2,10 @@ import tpred.db as db
 
 
 def run_report(minutes, filter_links=False):
+    print "Runing tweet report for", minutes, "minutes with link filter:", filter_links
+
     q = """
-    SELECT original.site_id, original.body_id, COUNT(*) as num_reposts, original.body, original.sn, MAX(original.diff), original.url, original.pic_url
+    SELECT original.site_id, original.body_id, COUNT(*) as num_reposts, original.site_post_id, original.body, original.sn, MAX(original.diff), original.url, original.pic_url
     FROM (
             SELECT p.site_id, pb.id as body_id, pb.body,sn.sn, p.site_post_id, CURRENT_TIMESTAMP-created_at as diff, url, sn.pic_url FROM post p JOIN post_body pb on p.body_id=pb.id
             JOIN sn ON p.sn_id=sn.id
@@ -26,14 +28,19 @@ def run_report(minutes, filter_links=False):
 
     res = db.session.execute(q)
     used = set()
-    for site_id, body_id, num_reposts, body, sn, diff, url, pic_url in res:
+    for site_id, body_id, num_reposts, site_post_id, body, sn, diff, url, pic_url in res:
         if body_id in used:
+            continue
+        if num_reposts <= 1:
             continue
 
         used.add(body_id)
 
         if url is None:
             url = ""
+
+        if site_id == 1:
+            url = 'https://twitter.com/' + sn + '/status/' + site_post_id
 
         yield {
             u'title': u"{} - {} - {} - {}".format(sn, num_reposts, diff, site_id),
