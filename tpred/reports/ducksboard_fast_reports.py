fc2581f983b42518a227714b099e85743c5b0dae
aelaguiz@gmail.com <aelaguiz@gmail.com>
2013-10-03 15:15:03 -0500

    Stuff

--- tpred/reports/ducksboard_fast_reports.py
+++ tpred/reports/ducksboard_fast_reports.py
@@ -3,8 +3,20 @@ import tpred.ducksboard as ducksboard
 
 
 if __name__ == '__main__':
-    data = list(trending_posts.run_report(15, False))
+    data = list(trending_posts.run_report(15, False))[:12]
     ducksboard.timeline(data, 237985)
 
-    data = list(trending_posts.run_report(60, True))
+    data = list(trending_posts.run_report(60, False))[:12]
+    ducksboard.timeline(data, 238075)
+
+    data = list(trending_posts.run_report(24 * 60, False))[:12]
+    ducksboard.timeline(data, 238078)
+
+    data = list(trending_posts.run_report(15, True))[:12]
+    ducksboard.timeline(data, 238079)
+
+    data = list(trending_posts.run_report(60, True))[:12]
     ducksboard.timeline(data, 238008)
+
+    data = list(trending_posts.run_report(24 * 60, True))[:12]
+    ducksboard.timeline(data, 238080)
